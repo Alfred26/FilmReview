339385809f3dd7924c62dcb19ae705393e38c567
Alfred26 <krappman@kth.se>
2013-03-08 12:14:21 +0100

    Added ignore to ignore compiled and buffer python files. Modified settings to support mysql-connection.

--- FilmReview/settings.py
+++ FilmReview/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', 'mysql'  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mysql-vt2013.csc.kth.se',                      # Or path to database file if using sqlite3.
+        'USER': 'krappmanadmin',                      # Not used with sqlite3.
+        'PASSWORD': 'pBzr17veud',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
